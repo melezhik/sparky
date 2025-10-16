2be4224c552c5677405f6a96c557874a74bf020a
Alexey Melezhik <melezhik@gmail.com>
2025-10-16 12:05:15 +0300

    fix

--- .tom/restart.raku
+++
@@ -1,1 +0,0 @@
-bash "sparman.raku  worker_ui stop && sparman.raku  worker_ui start";
--- .tom/tasks/sparkyui-restart/task.bash
+++ .tom/tasks/sparkyui-restart/task.bash
@@ -1,9 +1,5 @@
 set -e
 
-sparman  worker stop
-
-sparman --env SPARKY_TIMEOUT=10 worker start
-
 sparman  worker_ui stop
 
 sparman  worker_ui start
